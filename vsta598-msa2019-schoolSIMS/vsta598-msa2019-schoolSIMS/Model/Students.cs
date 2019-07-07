911439fef33ca7889d758b34e238a892d82a0a09
victors598 <victor.startsev@gmail.com>
2019-07-07 22:19:05 +1200

    Added screenshots of the SwaggerUI, api, Query Editor and Set Firewall

---
+++ vsta598-msa2019-schoolSIMS/vsta598-msa2019-schoolSIMS/Model/Students.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vsta598_msa2019_schoolSIMS.Model
+{
+    [Table("students")]
+    public partial class Students
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("first_name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Column("last_name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Column("phone_number")]
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+        [Column("email")]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [Column("dob", TypeName = "date")]
+        public DateTime? Dob { get; set; }
+        [Column("date_created", TypeName = "date")]
+        public DateTime? DateCreated { get; set; }
+        [Column("middle_names")]
+        [StringLength(100)]
+        public string MiddleNames { get; set; }
+        [Column("average_gpa")]
+        public double? AverageGpa { get; set; }
+    }
+}
---
+++ vsta598-msa2019-schoolSIMS/vsta598-msa2019-schoolSIMS/Model/vsta598_msa2019_schoolsims.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace vsta598_msa2019_schoolSIMS.Model
+{
+    public partial class vsta598_msa2019_schoolsims : DbContext
+    {
+        public vsta598_msa2019_schoolsims()
+        {
+        }
+
+        public vsta598_msa2019_schoolsims(DbContextOptions<vsta598_msa2019_schoolsims> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:vsta598-msa2019-sims.database.windows.net,1433;Initial Catalog=schoolSIMS;Persist Security Info=False;User ID=vsta598;Password=M0ngodbs;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.MiddleNames).IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber).IsUnicode(false);
+            });
+        }
+    }
+}
